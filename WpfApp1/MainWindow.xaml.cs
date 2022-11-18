1ea9272460916807aeba2b6cdb1377b6e7872b6a
zxx <357133157@qq.com>
2022-11-18 17:07:52 +0800

    添加项目文件。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            mygrid.DataContext = Mcl;
+          
+        }
+        public MyClass Mcl { get; set; } = new MyClass();
+
+        private Random rm = new Random();
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            string x;
+            x= rm.Next(0, 4).ToString();
+            for (int i = 0; i < 3; i++)
+            {
+                x += x;
+            }
+
+            Mcl.RichText = x.ToString();
+        }
+    }
+    public class MyClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private ICommand Command = new RoutedCommand();
+        private int line = 0;
+        private string richText;
+        public string RichText
+        {
+            get { return richText; }
+            set
+            {
+                line++;
+                if (line>100)
+                {
+                    line=0;
+                    richText = "";
+                }
+                richText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("RichText"));
+            }
+        }
+    }
+}
---
+++ WpfApp1/RichTextBoxHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace WpfApp1
+{
+    public class RichTextBoxHelper : DependencyObject
+    {
+        public static string GetRichText(DependencyObject obj)
+        {
+            return (string)obj.GetValue(RichTextProperty);
+        }
+
+        public static void SetRichText(DependencyObject obj, string value)
+        {
+            obj.SetValue(RichTextProperty, value);
+        }
+        public void AddMessage(string msg, Brush brush)
+        {
+            Run run = new Run();
+
+            Paragraph paragraph = new Paragraph();
+
+            run.Foreground = brush;
+
+            run.Text = msg;
+
+            paragraph.Inlines.Add(run);
+
+            paragraph.LineHeight = 1;
+
+            if (brush == System.Windows.Media.Brushes.Red)
+            {
+                paragraph.FontWeight = FontWeights.DemiBold;
+            }
+        }
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RichTextProperty =
+            DependencyProperty.RegisterAttached("RichText", typeof(string), typeof(RichTextBoxHelper), new FrameworkPropertyMetadata
+            {
+                BindsTwoWayByDefault = true,
+                PropertyChangedCallback = (obj, e) =>
+                {
+                    var richTextBox = (RichTextBox)obj;
+                    var text = GetRichText(richTextBox);
+                    Run run = new Run();
+                    Paragraph paragraph = new Paragraph();
+                    if (text.Contains("2"))
+                    {
+                        run.Foreground=Brushes.Red;
+                    }
+                    else if (text.Contains("1"))
+                    {
+                        run.Foreground = Brushes.Blue;
+                    }
+                    else if (text.Contains("3"))
+                    {
+                        run.Foreground = Brushes.Black;
+                    }
+                    else
+                    {
+                        run.Foreground = Brushes.DarkGray;
+                    }
+                    run.Text = text;
+                    paragraph.Inlines.Add(run);
+                    richTextBox.Document.Blocks.Add(paragraph);
+                    richTextBox.ScrollToEnd();
+                }
+            });
+    }
+}
