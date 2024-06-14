22b67fc437a4a599796a3d5158216f1a307ae25a
zxx <357133157@qq.com>
2024-06-14 16:16:11 +0800

    2

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XColorLog;
 
 namespace WpfApp1
 {
@@ -28,18 +29,27 @@ namespace WpfApp1
         {
             InitializeComponent();
             richTextBox.Document.Blocks.Clear();
-            XLog._richTextBox= richTextBox;
+            XLog = new XLog()
+            {
+                _richTextBox = richTextBox,
+                MaxLines = 500
+            };
         }
-        XColorLog.XLog XLog=new XColorLog.XLog();
+        XColorLog.XLog XLog;
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             // 模拟日志添加
             //AddLog(richTextBox,"This is a normal log.");
             //AddLog(richTextBox,"This is an alarm log!", LogLevel.Alarm);
-            XLog.AddLog("This is a normal log.",XColorLog.LogLevel.Information);
+            XLog.AddLog("This is a normal log.", XColorLog.LogLevel.Normal);
+            XLog.AddLog("This is a debug log.", XColorLog.LogLevel.Debug);
+            XLog.AddLog("This is a warning log.", XColorLog.LogLevel.Warning);
+            XLog.AddLog("This is a error log.", XColorLog.LogLevel.Error);
+            XLog.AddLog("This is a information log.", XColorLog.LogLevel.Information);
+            XLog.AddLog("This is an alarm log!", XColorLog.LogLevel.Alarm);
         }
 
-        public void AddLog(RichTextBox _richTextBox ,string logMessage, LogLevel logLevel = LogLevel.Normal)
+        public void AddLog(RichTextBox _richTextBox, string logMessage, LogLevel logLevel = LogLevel.Normal)
         {
             // 创建新的段落
             Paragraph paragraph = new Paragraph();
@@ -61,7 +71,7 @@ namespace WpfApp1
 
             // 将段落添加到RichTextBox的文档中
             _richTextBox.Document.Blocks.Add(paragraph);
-            if (_richTextBox.Document.Blocks.Count > 10)
+            if (_richTextBox.Document.Blocks.Count > 100)
             {
                 _richTextBox.Document.Blocks.Remove(richTextBox.Document.Blocks.FirstBlock);
             }
